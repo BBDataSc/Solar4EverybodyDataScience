2c3343e32fb1e14908aa5e7a1166b273b7186b6d
BuddeB <bjoern_budde@web.de>
2022-02-11 12:16:27 +0100

    Created a file that handles the data from the main file. It should produce a plt plot of all the prices received by the main file. #change1: pyplot has been referenced in the import message #change2: UserImputHandler.py will interact with the end-user thereby using the displayCollectedPrices.py file

---
+++ UserImputHandler.py
@@ -0,0 +1,19 @@
+# This file handles the questionaire to the user. Imputs will be provided by the user and it will process the info
+
+# Imported packages
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Variables
+a = []
+
+# Functions
+def UserImput():
+    # do something
+    x = 2
+    return(x)
+
+if __name__ == "__main__":
+    print(UserImput())
